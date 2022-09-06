12ab13fe6f9f5d4fb6d974a55cf1db7a556a97d0
roman <rubromvit@gmail.com>
2022-09-06 20:39:50 +0300

    init fix

--- ewm-service/src/test/java/ru/rubik/ewmservice/EwmServiceApplicationTests.java
+++ ewm-service/src/test/java/ru/rubik/ewmservice/EwmServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.yandex.mainservice;
+package ru.rubik.ewmservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MainServiceApplicationTests {
+class EwmServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
